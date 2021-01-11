29b0cd02b0f6b789cc6ec3a90556db4fecaf1af9
vo van thanh <thanh@Macs-MacBook-Pro.local>
2021-01-12 00:49:15 +0700

    add logout

--- app/controllers/api/v1/sessions_controller.rb
+++ app/controllers/api/v1/sessions_controller.rb
@@ -1,7 +1,10 @@
 class Api::V1::SessionsController < Devise::SessionsController
   before_action :sign_in_params, only: :create
   before_action :load_user, only: :create
+  before_action :valid_token, only: :destroy
+  skip_before_action :verify_signed_out_user, only: :destroy
 
+  # Sign in
   def create
     if @user.valid_password?(sign_in_params[:password])
       sign_in "user", @user
@@ -9,6 +12,13 @@ class Api::V1::SessionsController < Devise::SessionsController
     else
       json_response "Unauthorized", false, {}, :unauthorized
     end 
+  end
+
+  # Log out
+  def destroy
+    sign_out @user
+    @user.generate_new_authentication_token
+    json_response "Log out Successfully", true, {}, :ok
   end
 
   private
@@ -21,7 +31,16 @@ class Api::V1::SessionsController < Devise::SessionsController
     if @user
       return @user
     else
-      json_response "Cannot get User", false, {}, :failure
+      json_response "Cannot get User", false, {}, :bad_request
+    end
+  end
+
+  def valid_token
+    @user = User.find_by authentication_token: request.headers["AUTH-TOKEN"]
+    if @user
+      return @user
+    else
+      json_response "Invalid Token", false, {}, :bad_request
     end
   end
 end
